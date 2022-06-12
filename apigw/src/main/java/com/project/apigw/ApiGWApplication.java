c2d8660a0acc6de00d5710d76abf5fe4b882c258
Gonçalo Pimenta <goncalopimenta@GoncalosMacbook.Home>
2022-06-12 22:15:12 +0100

    trying to add gateway, but have problems with the hostname

---
+++ apigw/src/main/java/com/project/apigw/ApiGWApplication.java
@@ -0,0 +1,15 @@
+package com.project.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
+@SpringBootApplication (exclude={DataSourceAutoConfiguration.class})
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class,args);
+
+    }
+}
